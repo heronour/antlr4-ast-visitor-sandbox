73055c654ff5aa7e3cb7021d26ca565a450b67a5
oliver.zeigermann <oliver.zeigermann@gmail.com>
2012-01-22 08:23:31 +0000

    Update to latest ANTLR version

--- src/parser/v4/BlankExprV4ASTCreationListener.java
+++ src/parser/v4/BlankExprV4ASTCreationListener.java
@@ -9,14 +9,14 @@ import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.Token;
 
 public class BlankExprV4ASTCreationListener implements ExprV4ASTCreationListener {
+	@Override public void enterRule(ExprV4ASTCreationParser.atomContext ctx) { }
+	@Override public void exitRule(ExprV4ASTCreationParser.atomContext ctx) { }
+
 	@Override public void enterRule(ExprV4ASTCreationParser.startContext ctx) { }
 	@Override public void exitRule(ExprV4ASTCreationParser.startContext ctx) { }
 
 	@Override public void enterRule(ExprV4ASTCreationParser.exprContext ctx) { }
 	@Override public void exitRule(ExprV4ASTCreationParser.exprContext ctx) { }
-
-	@Override public void enterRule(ExprV4ASTCreationParser.atomContext ctx) { }
-	@Override public void exitRule(ExprV4ASTCreationParser.atomContext ctx) { }
 
 	@Override public void enterEveryRule(ParserRuleContext<Token > ctx) { }
 	@Override public void exitEveryRule(ParserRuleContext<Token > ctx) { }
