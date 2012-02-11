4b521d1e0470659f2bf0d57a2ca2b7f1647c5e29
oliver.zeigermann@gmail.com <oliver.zeigermann@gmail.com@57e3d961-5d06-d14e-011f-a849e23bff55>
2012-02-11 11:05:07 +0000

    Contexts of visitor example simplified

--- trunk/src/de/zeigermann/compilerSandbox/parser/v4/byhand/ExprV4Visitor.java
+++ trunk/src/de/zeigermann/compilerSandbox/parser/v4/byhand/ExprV4Visitor.java
@@ -8,12 +8,8 @@ import de.zeigermann.compilerSandbox.parser.v4.byhand.ExprV4Parser.startContext;
 
 public interface ExprV4Visitor {
 	void visit(opExprContext ctx);
-
 	void visit(startContext ctx);
-
 	void visit(atomExprContext ctx);
-
 	void visit(exprContext ctx);
-
 	void visit(parenExprContext ctx);
 }
